cad6c6dd9e9ed3e8570db76f4eb537df380a0224
AxolDeloveper <117906724+AxolDeloveper@users.noreply.github.com>
2022-11-10 11:12:27 +0000

    New Update
    Added 2 New Pages

---
+++ header.php
@@ -0,0 +1,5 @@
+<?php get_navbar(); ?>
+<header>
+<center><img src="./image/Intro.png"></center>
+</header>
+<?php get_content(); ?>
---
+++ navbar.php
@@ -0,0 +1,8 @@
+<?php get_main(); ?>
+<ul id="navbar">
+      <li><a href="#">Главная</a></li>
+      <li><a href="./pages/Objects/list.php">Список Объектов</a></li>
+      <li><a href="#">Контакты</a></li>
+      <li><a href="./pages/aboutMe.php">О нас</a></li>
+    </ul>
+<?php get_header(); ?>
