8de0d6236e8b075828f677e5eb940033e2e18c41
AxolDeloveper <117906724+AxolDeloveper@users.noreply.github.com>
2022-11-10 10:24:23 +0000

    Site Pages
    New Pages PHP

---
+++ Сайт/pages/Objects/SCP-035/main.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>SCP-035</title>
+</head>
+<body>
+<div SCP063 style="left: 100; right: 400;" /div>
+</body>
+</html>
