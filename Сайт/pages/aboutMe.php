d1ee69f58a24a71656e6a3b2310285447d1e86bc
AxolDeloveper <117906724+AxolDeloveper@users.noreply.github.com>
2022-11-18 12:06:07 +0000

    Site New Update 1.2

--- Сайт/pages/aboutMe.php
+++ Сайт/pages/aboutMe.php
@@ -4,8 +4,11 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
+	
 </head>
 <body>
-<p>ash</p>
+	<h1><center><p>Создатель Сайта</p></center></h1>
+<p><h1>Здраствуй Пользователь! Меня Зовут Axol И я Являюсь создателем этого сайта! Код, Информацию о сайте вы можете посмотреть на сайте Или На GitHub</h1></p>
 </body>
+<?php get_Contacts(); ?>
 </html>
---
+++ Сайт/pages/page1 — копия.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+</head>
+<body>
+
+<header>
+    <style>
+      #navbar {
+        margin: 0;
+        padding: 0;
+        list-style-type: none;
+        border: 2px solid #0066FF;
+        border-radius: 20px 5px;
+        width: 550px;
+        text-align: center;
+        background-color: #33ADFF;
+      }
+      #navbar li { display: inline; }
+      #navbar a {
+        color: #fff;
+        padding: 5px 10px;
+        text-decoration: none;
+        font-weight: bold;
+        display: inline-block;
+        width: 100px;
+      }
+      #navbar a:hover {
+        border-radius: 20px 5px;
+        background-color: #0066FF;
+      }
+      .content {
+        background: #F0E68C;
+      }
+    </style>
+  </head>
+ 
+  
+  
+    <ul id="navbar">
+      <li><a href="#">Главная</a></li>
+      <li><a href="#">Новости</a></li>
+      <li><a href="#">Контакты</a></li>
+      <li><a href="#">О нас</a></li>
+    </ul>
+
+
+ </header>
+ <div class="content" style="height: 100px; width: 100px;">
+   <content>
+
+      <p><h1>Выберите SCP Объект:</h1></p>
+      <p><h2><a href="./SCP-035/main.php">1. SCP-035</a></h2></p>
+      <p><h2><a href="./SCP-1371/main.php">2. SCP-1371</a></h2></p>
+      <p><h2><a href="./SCP-173/main.php">3. SCP-173</a></h2></p>
+      <p><h2><a href="./SCP-682/main.php">4. SCP-682</a></h2></p>
+      <p><h2><a href="./SCP-096/main.php">5. SCP-096</a></h2></p>
+   </content>
+  </div>
+</body>
+</html>
